4f3323713cda7a2db0d0bca02184f24bfc8bd863
Beto Toro <betotoro0902@gmail.com>
2022-08-10 10:37:36 -0500

    [feat]: add the instafilter layout

--- Instafilter/ContentView.swift
+++ Instafilter/ContentView.swift
@@ -8,9 +8,50 @@
 import SwiftUI
 
 struct ContentView: View {
+  @State private var image: Image?
+  @State private var filterIntensity = 0.5
+  
+  func save() {
+  }
   
   var body: some View {
-    ImagePickerView()
+    NavigationView {
+      VStack {
+        ZStack {
+          Rectangle()
+            .fill(.secondary)
+          
+          Text("Tap to select a picture")
+            .foregroundColor(.white)
+            .font(.headline)
+          
+          image?
+            .resizable()
+            .scaledToFit()
+        }
+        .onTapGesture {
+          // select an image
+        }
+        
+        HStack {
+          Text("Intensity")
+          Slider(value: $filterIntensity)
+        }
+        .padding(.vertical)
+        
+        HStack {
+          Button("Change Filter") {
+            // change filter
+          }
+          
+          Spacer()
+          
+          Button("Save", action: save)
+        }
+      }
+      .padding([.horizontal, .bottom])
+      .navigationTitle("Instafilter")
+    }
   }
 }
 
